a218e3d3ceee0ca398627b18eb94421c65c63a13
Mohit Arora <mohit.arora1902@gmail.com>
2022-02-02 18:30:39 +0530

    Fix str_random

--- database/factories/GroupsModelFactory.php
+++ database/factories/GroupsModelFactory.php
@@ -17,9 +17,9 @@ $factory->define(Mohiqssh\Groups\Models\User::class, function (Faker $faker) {
     static $password;
 
     return [
-        'name'           => $faker->name,
-        'email'          => $faker->unique()->safeEmail,
-        'password'       => $password ?: $password = bcrypt('secret'),
-        'remember_token' => str_random(10),
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'password' => $password ?: $password = bcrypt('secret'),
+        'remember_token' => \Illuminate\Support\Str::random(10),
     ];
 });
